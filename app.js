3c2aae887866482ff01e7f0c8b91c0263b874dc7
Rebecca Mello <rebeccabmello@gmail.com>
2024-08-07 12:17:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag,texto){
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
 }
 function exibirMensagemInicial(){
-    exibirTextoNaTela('h1', 'Jogo do numero secreto');
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');   
 }
 function gerarNumeroAleatorio(){
